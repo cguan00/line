7ab82db7d80e035f8f3aabc0a23f5788d663adce
Christy Guan <cguan00@stuy.edu>
2020-02-10 10:30:31 -0500

    removed old testing code

--- main.py
+++ main.py
@@ -6,71 +6,68 @@ from random import randint
 s = new_screen()
 c = [ 0, 255, 0 ]
 
-#octants 1 and 5
-draw_line(0, 0, XRES-1, YRES-1, s, c)
-draw_line(0, 0, XRES-1, YRES / 2, s, c)
-draw_line(XRES-1, YRES-1, 0, 250, s, c)
+# #octants 1 and 5
+# draw_line(0, 0, XRES-1, YRES-1, s, c)
+# draw_line(0, 0, XRES-1, YRES / 2, s, c)
+# draw_line(XRES-1, YRES-1, 0, 250, s, c)
+#
+# #octants 8 and 4
+# c[BLUE] = 255;
+# draw_line(0, YRES-1, XRES-1, 0, s, c);
+# draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
+# draw_line(XRES-1, 0, 0, 250, s, c);
+#
+# #octants 2 and 6
+# c[RED] = 255;
+# c[GREEN] = 0;
+# c[BLUE] = 0;
+# draw_line(0, 0, XRES/2, YRES-1, s, c);
+# draw_line(XRES-1, YRES-1, 250, 0, s, c);
+#
+# #octants 7 and 3
+# c[BLUE] = 255;
+# draw_line(0, YRES-1, XRES/2, 0, s, c);
+# draw_line(XRES-1, 0, 250, YRES-1, s, c);
+#
+# #horizontal and vertical
+# c[BLUE] = 0;
+# c[GREEN] = 255;
+# draw_line(0, 250, XRES-1, 250, s, c);
+# draw_line(250, 0, 250, YRES-1, s, c);
+#
 
-#octants 8 and 4
-c[BLUE] = 255;
-draw_line(0, YRES-1, XRES-1, 0, s, c);
-draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
-draw_line(XRES-1, 0, 0, 250, s, c);
+seed(2)
 
-#octants 2 and 6
-c[RED] = 255;
-c[GREEN] = 0;
-c[BLUE] = 0;
-draw_line(0, 0, XRES/2, YRES-1, s, c);
-draw_line(XRES-1, YRES-1, 250, 0, s, c);
 
-#octants 7 and 3
-c[BLUE] = 255;
-draw_line(0, YRES-1, XRES/2, 0, s, c);
-draw_line(XRES-1, 0, 250, YRES-1, s, c);
+for i in range(100):
+    c[RED] = 255
+    c[BLUE] = randint(0,255)
+    c[GREEN] = randint(0,255)
 
-#horizontal and vertical
-c[BLUE] = 0;
-c[GREEN] = 255;
-draw_line(0, 250, XRES-1, 250, s, c);
-draw_line(250, 0, 250, YRES-1, s, c);
+    draw_line(100, 100, 100 + randint(-150, 150), 100 + randint(-150, 150), s, c)
 
-c[RED] = 0
-c[GREEN] = 128
-c[BLUE] = 255
-#
-# seed(2)
-#
-#
-# for i in range(100):
-#     c[RED] = 255
-#     c[BLUE] = randint(0,255)
-#     c[GREEN] = randint(0,255)
-#
-#     draw_line(100, 100, 100 + randint(-150, 150), 100 + randint(-150, 150), s, c)
-#
-#
-# for i in range(100):
-#     c[RED] = randint(0,255)
-#     c[BLUE] = 255
-#     c[GREEN] = randint(0,255)
-#
-#     draw_line(100, 400, 100 + randint(-150, 150), 400 + randint(-150, 150), s, c)
-#
-#
-# for i in range(100):
-#     c[RED] = randint(0,255)
-#     c[BLUE] = randint(0,255)
-#     c[GREEN] = 255
-#
-#     draw_line(400, 100, 400 + randint(-150, 150), 100 + randint(-150, 150), s, c)
-#
-# for i in range(100):
-#     c[RED] = 255
-#     c[BLUE] = randint(0,255)
-#     c[GREEN] = randint(0,255)
-#
-#     draw_line(400, 400, 400 + randint(-150, 150), 400 + randint(-150, 150), s, c)
+
+for i in range(100):
+    c[RED] = randint(0,255)
+    c[BLUE] = 255
+    c[GREEN] = randint(0,255)
+
+    draw_line(100, 400, 100 + randint(-150, 150), 400 + randint(-150, 150), s, c)
+
+
+for i in range(100):
+    c[RED] = randint(0,255)
+    c[BLUE] = randint(0,255)
+    c[GREEN] = 255
+
+    draw_line(400, 100, 400 + randint(-150, 150), 100 + randint(-150, 150), s, c)
+
+for i in range(100):
+    c[RED] = 255
+    c[BLUE] = randint(0,255)
+    c[GREEN] = randint(0,255)
+
+    draw_line(400, 400, 400 + randint(-150, 150), 400 + randint(-150, 150), s, c)
 
 
 display(s)
